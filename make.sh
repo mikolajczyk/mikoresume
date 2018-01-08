b6b7e8e4a9258cc0af45dc8c1f9034fd3ca03f8e
Matt Mikolajczyk <mmikolajczyk1111@gmail.com>
2018-01-07 16:18:03 -0800

    added word doc

--- make.sh
+++ make.sh
@@ -4,4 +4,5 @@ pandoc --from markdown --to markdown master.md > README.md
 
 pandoc --from markdown --to plain master.md > MattMikolajczykResume.txt
 pandoc --from markdown --to html5 master.md > MattMikolajczykResume.html
+pandoc --from markdown --to docx master.md -o MattMikolajczykResume.docx
 echo "Dunzo! <( ^_^ )>"
