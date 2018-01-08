1ba037f8137f9d857941e254d1b2c4910a22be36
Matt Mikolajczyk <mmikolajczyk1111@gmail.com>
2018-01-07 16:38:32 -0800

    got rid of from command in make file

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 echo "Creating files..."
 pandoc --from markdown --to markdown master.md > README.md
 
-pandoc --from markdown --to plain master.md > MattMikolajczykResume.txt
-pandoc --from markdown --to html5 master.md > MattMikolajczykResume.html
-pandoc --from markdown --to docx master.md -o MattMikolajczykResume.docx
+pandoc -s master.md -o  MattMikolajczykResume.txt
+pandoc -s master.md -o  MattMikolajczykResume.html
+pandoc -s master.md -o  MattMikolajczykResume.docx
 echo "Dunzo! <( ^_^ )>"
