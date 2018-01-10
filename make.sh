de0631abd1bade82a37de5051d48063cb56eef02
Matt Mikolajczyk <mmikolajczyk1111@gmail.com>
2018-01-10 15:16:05 -0800

    new chibi face

--- make.sh
+++ make.sh
@@ -8,4 +8,5 @@ pandoc -s master.md -o README.md
 pandoc -s master.md -o  MattMikolajczykResume.txt
 pandoc -s master.md -o  MattMikolajczykResume.html
 pandoc -s master.md -o  MattMikolajczykResume.docx
-echo "Dunzo! <( ^_^ )>"
+echo "Dunzo! (〃^∇^)ﾉ "
+
